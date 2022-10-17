f5fae09137eca4e7d0e6cded505a046d235d08d2
ronald <ronaldsito100@gmail.com>
2022-10-17 09:35:33 -0500

    aprendiendo foca y match

--- estructuraDecicion/estructurade_decicion.py
+++ estructuraDecicion/estructurade_decicion.py
@@ -2,12 +2,47 @@
 ## if else, permite que un programa ejecute unas instrucciones cuando se cumple una condición y otras instrucciones cuando no se cumple esa condición.
 ## elif después de su declaración if inicial.Ten en cuenta que solo se ejecutará una condición:
 
-comida_preferida = "Tacos"
-if comida_preferida == "Tacos":
-	print("Te gustan los Tacos")
-elif comida_preferida == "Pizza":
-	print("Te gusta la Pizza")
-elif comida_preferida == "Hamburguesa":
-	print("Te gustan las hamburguesas")
-else:
-	print("Te gusta otra comida")
+# comida_preferida = "Tacos"
+# if comida_preferida == "Tacos":
+# 	print("Te gustan los Tacos")
+# elif comida_preferida == "Pizza":
+# 	print("Te gusta la Pizza")
+# elif comida_preferida == "Hamburguesa":
+# 	print("Te gustan las hamburguesas")
+# else:
+# 	print("Te gusta otra comida")
+
+
+##ejemplo de match
+
+# operación="resta"
+# match operacion:
+# 	case "suma":
+#        print("realizare una suma")
+# 	case "resta":
+#        print("realizare una resta")
+# 	case "multiplicacion":
+#        print("realizare una multiplicacion")
+# 	case _:
+#        print("no hay operacion")
+
+## tendremos una variable con el mensaje hola mundo 
+## perdir al usuario un texto si el texto es hola mostraras el mensaje completo 
+## si el texto ingeresado es como estas mostraras el mensaje hola si el texto 
+## ingresado es saludos estraeras del mensaje la palabra mundo
+## si se ingresa otro texto mostraras por defecto el mensaje de error 
+
+mensaje="hola mundo"
+texto=input("ingresa un texto:")
+match texto:
+  case "hola":
+     print(mensaje[:])
+  case "como estas":
+     print (mensaje[:4])
+  case "saludos":
+     print(mensaje[5:])
+  case _:
+     print("error")
+
+
+
--- operadores/operadores.py
+++ operadores/operadores.py
@@ -1,20 +1,26 @@
-## Considera la expresión, “suma = 21 + 10”. Aquí, 2 y 3 son los operandos y + es el operador aritmético. El resultado de la operación se almacena en la variable suma
-a = 21
-b = 10
-c = a + b
-print "el valor de c es:", c
-## Considera la expresión, “resta = 14 - 7”. Aquí, 2 y 3 son los operandos y - es el operador aritmético. El resultado de la operación se almacena en la variable resta
-a = 14
-b = 7
-c = a - b
-print "el valor de c es:", c
-## Considera la expresión, “multiplicacion = 2 * 3”. Aquí, 2 y 3 son los operandos y * es el operador aritmético. El resultado de la operación se almacena en la variable multiplicacion
-a = 2
-b = 3
-c = a * b
-print "el valor de c es:", c
-## Considera la expresión, “division = 5 / 25”. Aquí, 2 y 3 son los operandos y / es el operador aritmético. El resultado de la operación se almacena en la variable division
-a = 5
-b = 25
-c = a / b
-print "el valor de c es:", c
+# ## Considera la expresión, “suma = 21 + 10”. Aquí, 2 y 3 son los operandos y + es el operador aritmético. El resultado de la operación se almacena en la variable suma
+# a = 21
+# b = 10
+# c = a + b
+# print "el valor de c es:", c
+# ## Considera la expresión, “resta = 14 - 7”. Aquí, 2 y 3 son los operandos y - es el operador aritmético. El resultado de la operación se almacena en la variable resta
+# a = 14
+# b = 7
+# c = a - b
+# print "el valor de c es:", c
+# ## Considera la expresión, “multiplicacion = 2 * 3”. Aquí, 2 y 3 son los operandos y * es el operador aritmético. El resultado de la operación se almacena en la variable multiplicacion
+# a = 2
+# b = 3
+# c = a * b
+# print "el valor de c es:", c
+# ## Considera la expresión, “division = 5 / 25”. Aquí, 2 y 3 son los operandos y / es el operador aritmético. El resultado de la operación se almacena en la variable division
+# a = 5
+# b = 25
+# c = a / b
+# print "el valor de c es:", c
+
+## operador foca 
+
+texto="hola mundo"
+print(texto[:4])
+
